485eef62b459b9e3e478b0aa1642c627d7ff5330
nilesh <nileshv@cybage.com>
2023-06-12 18:55:07 +0530

    initial commit

---
+++ MVC_Project/Controllers/Machine_Details.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MVC_Project.Controllers
+{
+    public class Machine_Details : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public Machine_Details(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+            {
+
+                var response = await _httpClient.GetAsync("https://localhost:7227/api/Machine");
+                var resultString = await response.Content.ReadAsStringAsync();
+                dynamic result = JsonConvert.DeserializeObject(resultString);
+                return View(result);
+            }
+        
+    }
+}
