5a352192888e3c5a11230a302884cd8b5527a9e0
nilesh <nileshv@cybage.com>
2023-06-13 12:16:18 +0530

    exception handled

--- MVC_Project/Controllers/Machine_Details.cs
+++ MVC_Project/Controllers/Machine_Details.cs
@@ -13,13 +13,31 @@ namespace MVC_Project.Controllers
         }
 
         public async Task<IActionResult> Index()
+        {
+            try
             {
-
                 var response = await _httpClient.GetAsync("https://localhost:7227/api/Machine");
                 var resultString = await response.Content.ReadAsStringAsync();
                 dynamic result = JsonConvert.DeserializeObject(resultString);
                 return View(result);
             }
+            catch (Exception ex)
+            {
+                // Handle the exception here
+                // You can log the exception details, display a user-friendly error message, or take appropriate action
+                Console.WriteLine($"An exception occurred: {ex.Message}");
+                // Optionally, you can return an error view or redirect to an error page
+                return View("Error");
+            }
+        }
         
+
+
+
+
+
+
+
+
     }
 }
